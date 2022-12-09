6f46506a0658bc77e2e492e7090c48c84be3fbac
user name <user email>
2022-12-09 15:55:01 +0000

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 """ Script return json"""
 from api.v1.views import app_views
 from flask import jsonify
-import models
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -31,17 +31,13 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats_objtects():
     """return amount of objet for class"""
-    classes = {
-        "amenities": Amenity,
-        "cities": City,
-        "places": Place,
-        "reviews": Review,
-        "states": State,
-        "users": User
-        }
-    new_dict = {}
-    for key, value in classes.items():
-        cont = models.storage.count(value)
-        new_dict[key] = cont
+    obj_res = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
     
-    return jsonify(new_dict)
+    return jsonify(obj_res)
