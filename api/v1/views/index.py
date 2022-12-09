6f1ad3e21cc46c2fa5446aca63638b23fda68dd7
user name <user email>
2022-12-09 05:03:55 +0000

    add method for count class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,37 @@
 """ Script return json"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 dict_json = {"status": "OK"}
+
+classes = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+        }
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     return (jsonify(dict_json))
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats_objtects():
+    """return amount of objet for class"""
+    new_dict = {}
+    for key, value in classes.items():
+        count = storage.count(value)
+        new_dict[key] = count
+    return jsonify(new_dict)
