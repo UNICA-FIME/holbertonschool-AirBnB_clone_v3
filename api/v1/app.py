9003df29c49491a2a99cccc3a844467135092e2a
user name <user email>
2022-12-09 23:12:08 +0000

    display error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Script what run the app"""
 from flask import Flask
+from flask import make_response
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 from flask import Blueprint
@@ -15,6 +17,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """This is method call the method close"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Register a function to handle errors by code"""
+    return (make_response(jsonify({"error": "Not found"}), 404))
 
 
 if __name__ == "__main__":
